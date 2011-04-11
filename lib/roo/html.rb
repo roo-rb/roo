149d5be5f64d283e2cd0f05b1be7d3d82fb415ef
Ben Hundley <ben.hundley@gmail.com>
2011-04-11 12:26:26 -0500

    html might be working?

--- lib/roo/html.rb
+++ lib/roo/html.rb
@@ -1,23 +1,34 @@
-module Roo
-  class Spreadsheet
-    class HTML
+class HTML < GenericSpreadsheet
   
-      def initialize(filename)
-        raw = File.open(filename).read
-        table = raw.match(/<table>(.*)<\/table>/)[1] rescue raise('No <table> found.')
-        @doc = Nokogiri::HTML.open(table)
-        @cell = {}
-        parse_html
+  def initialize(filename)
+    raw = File.open(filename).read
+    table = raw.match(/<table>(.*)<\/table>/)[1] rescue raise('No <table> found.')
+    @sheets = [(@default_sheet = 'Table')]
+    @cell = {@default_sheet => {}}
+    parse_html(table)
+    @cells_read = {@default_sheet => true}
+    @first_row = Hash.new
+    @last_row = Hash.new
+    @first_column = Hash.new
+    @last_column = Hash.new
+    @header_line = 1
+  end
+
+  def parse_html(raw_string)
+    doc = Nokogiri::HTML(raw_string)
+    doc.xpath('//tr').each_with_index do |row,row_no|
+      row.children.each_with_index do |col,col_no|
+        @cell[@default_sheet][[row_no + 1,col_no + 1]] = col.text
       end
-  
-      def parse_html
-        @doc.xpath('//tr').each_with_index do |row,row_no|
-          row.children.each_with_index do |col,col_no|
-            @cell[[row_no,col_no]] = col.text
-          end
-        end
-      end
-      
     end
   end
+
+  def row(index)
+    @cell[@default_sheet].select {|k,v| k[0] == index}.values
+  end
+  
+  def cell(row_no,col_no)
+    @cell[@default_sheet][[row_no,col_no]]
+  end
+      
 end
