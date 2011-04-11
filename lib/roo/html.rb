67620577ea04a403415b15200cdb417dd45dde15
Ben Hundley <ben.hundley@gmail.com>
2011-04-11 11:52:42 -0500

    adjusting HTML class

--- lib/roo/html.rb
+++ lib/roo/html.rb
@@ -1,19 +1,23 @@
-class Roo::Spreadsheet::HTML
+module Roo
+  class Spreadsheet
+    class HTML
   
-  def initialize(filename)
-    raw = File.open(filename).read
-    table = raw.match(/<table>(.*)<\/table>/)[1] rescue raise('No <table> found.')
-    @doc = Nokogiri::HTML.open(table)
-    @cell = {}
-    parse_html
-  end
-  
-  def parse_html
-    @doc.xpath('//tr').each_with_index do |row,row_no|
-      row.children.each_with_index do |col,col_no|
-        @cell[[row_no,col_no]] = col.text
+      def initialize(filename)
+        raw = File.open(filename).read
+        table = raw.match(/<table>(.*)<\/table>/)[1] rescue raise('No <table> found.')
+        @doc = Nokogiri::HTML.open(table)
+        @cell = {}
+        parse_html
       end
+  
+      def parse_html
+        @doc.xpath('//tr').each_with_index do |row,row_no|
+          row.children.each_with_index do |col,col_no|
+            @cell[[row_no,col_no]] = col.text
+          end
+        end
+      end
+      
     end
   end
-  
 end
