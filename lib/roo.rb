74c559d4c878996f0d76b6fefcfef535b1ef65c6
Ben Woosley <ben.woosley@gmail.com>
2012-03-19 12:25:47 -0700

    Remove a bunch of extraneous methods from Roo: .libpath, .path, and .require_all_libs_relative_to

--- lib/roo.rb
+++ lib/roo.rb
@@ -1,46 +1,6 @@
 module Roo
 
-  # :stopdoc:
   VERSION = '1.10.1'
-  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
-  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  # :startdoc:
-
-  # Returns the version string for the library.
-  #
-  def self.version
-    VERSION
-  end
-
-  # Returns the library path for the module. If any arguments are given,
-  # they will be joined to the end of the libray path using
-  # <tt>File.join</tt>.
-  #
-  def self.libpath( *args )
-    args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
-  end
-
-  # Returns the lpath for the module. If any arguments are given,
-  # they will be joined to the end of the path using
-  # <tt>File.join</tt>.
-  #
-  def self.path( *args )
-    args.empty? ? PATH : ::File.join(PATH, args.flatten)
-  end
-
-  # Utility method used to require all files ending in .rb that lie in the
-  # directory below this file that has the same name as the filename passed
-  # in. Optionally, a specific _directory_ name can be passed in such that
-  # the _filename_ does not have to be equivalent to the directory.
-  #
-  def self.require_all_libs_relative_to( fname, dir = nil )
-    dir ||= ::File.basename(fname, '.*')
-    search_me = ::File.expand_path(
-      ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
-    Dir.glob(search_me).sort.each {|rb|
-      puts "DEBUG: require #{rb}"
-      require rb}
-  end
 
   class Spreadsheet
     class << self
@@ -65,7 +25,6 @@ module Roo
     end
   end
 
-  # autoload :Spreadsheetparser, 'roo/spreadsheetparser' TODO:
   autoload :GenericSpreadsheet, 'roo/generic_spreadsheet'
   autoload :Openoffice,         'roo/openoffice'
   autoload :Excel,              'roo/excel'
@@ -75,8 +34,4 @@ module Roo
 
   autoload :Excel2003XML,       'roo/excel2003xml'
   autoload :RooRailsHelper,     'roo/roo_rails_helper'
-end  # module Roo
-
-#Roo.require_all_libs_relative_to(__FILE__)
-
-# EOF
+end
