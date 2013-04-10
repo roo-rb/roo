44b7ba00056036eb630ee9353dd03bc83fb5bc62
Ben Woosley <ben.woosley@gmail.com>
2013-04-10 03:06:54 -0700

    Drop mistaken autoload for RooRailsHelper - that module doesn't exist so the file should be required directly.

--- lib/roo.rb
+++ lib/roo.rb
@@ -3,14 +3,12 @@ module Roo
   VERSION = '1.10.3'
 
   autoload :Spreadsheet, 'roo/spreadsheet'
-
   autoload :GenericSpreadsheet, 'roo/generic_spreadsheet'
+
   autoload :Openoffice,         'roo/openoffice'
   autoload :Excel,              'roo/excel'
   autoload :Excelx,             'roo/excelx'
+  autoload :Excel2003XML,       'roo/excel2003xml'
   autoload :Google,             'roo/google'
   autoload :Csv,                'roo/csv'
-
-  autoload :Excel2003XML,       'roo/excel2003xml'
-  autoload :RooRailsHelper,     'roo/roo_rails_helper'
 end
