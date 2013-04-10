2954420d7580c2d847c2e65a40f334ab9a8529a5
Ben Woosley <ben.woosley@gmail.com>
2013-04-10 01:27:42 -0700

    Move Roo::Spreadsheet into its own file.

--- lib/roo.rb
+++ lib/roo.rb
@@ -2,29 +2,7 @@ module Roo
 
   VERSION = '1.10.3'
 
-  class Spreadsheet
-    class << self
-      def open(file)
-        file = File === file ? file.path : file
-        case File.extname(file)
-        when '.xls'
-          Roo::Excel.new(file)
-        when '.xlsx'
-          Roo::Excelx.new(file)
-        when '.ods'
-          Roo::Openoffice.new(file)
-        when '.xml'
-          Roo::Excel2003XML.new(file)
-        when ''
-          Roo::Google.new(file)
-        when '.csv'
-          Roo::Csv.new(file)
-        else
-          raise ArgumentError, "Don't know how to open file #{file}"
-        end
-      end
-    end
-  end
+  autoload :Spreadsheet, 'roo/spreadsheet'
 
   autoload :GenericSpreadsheet, 'roo/generic_spreadsheet'
   autoload :Openoffice,         'roo/openoffice'
---
+++ lib/roo/spreadsheet.rb
@@ -0,0 +1,25 @@
+module Roo
+  class Spreadsheet
+    class << self
+      def open(file)
+        file = File === file ? file.path : file
+        case File.extname(file)
+        when '.xls'
+          Roo::Excel.new(file)
+        when '.xlsx'
+          Roo::Excelx.new(file)
+        when '.ods'
+          Roo::Openoffice.new(file)
+        when '.xml'
+          Roo::Excel2003XML.new(file)
+        when ''
+          Roo::Google.new(file)
+        when '.csv'
+          Roo::Csv.new(file)
+        else
+          raise ArgumentError, "Don't know how to open file #{file}"
+        end
+      end
+    end
+  end
+end
