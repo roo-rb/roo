1b1eb3252e3d02bca86c882860c47a1a2bad1089
Steven Daniels <stevendaniels88@gmail.com>
2015-07-09 06:26:12 +0800

    Replace hard tabs with soft tabs

--- test/all_ss.rb
+++ test/all_ss.rb
@@ -1,11 +1,12 @@
 require 'roo'
-Dir.glob("test/files/*.ods").each do |fn|
-	begin
-		oo = Roo::OpenOffice.new fn
-		print File.basename(fn) + " "
-		puts oo.officeversion
-	rescue Zip::ZipError, Errno::ENOENT => e
-		# file is not a real .ods spreadsheet file
-		puts e.message
-	end
+
+Dir.glob('test/files/*.ods').each do |fn|
+  begin
+    oo = Roo::OpenOffice.new fn
+    print "#{File.basename(fn)} "
+    puts oo.officeversion
+  rescue Zip::ZipError, Errno::ENOENT => e
+    # file is not a real .ods spreadsheet file
+    puts e.message
+  end
 end
