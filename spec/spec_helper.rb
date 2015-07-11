9cb6b6898cf00f7332572b2ed8869df17ba45b2b
Pablo Herrero <pablodherrero@gmail.com>
2015-07-10 23:21:05 -0300

    Convert generic spreadsheet tests to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'simplecov'
 require 'roo'
-
 require 'vcr'
+
+require 'helpers'
+
+RSpec.configure do |c|
+  c.include Helpers
+end
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
