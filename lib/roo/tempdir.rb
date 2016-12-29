2310ae7cd2a6cb9cc4d1247c3413690705128f15
Steven Daniels <stevendaniels88@gmail.com>
2016-12-29 15:15:16 -0500

    Refactored Roo::Tempdir

--- lib/roo/tempdir.rb
+++ lib/roo/tempdir.rb
@@ -2,7 +2,7 @@ module Roo
   module Tempdir
     def finalize_tempdirs(object_id)
       if @tempdirs && (dirs_to_remove = @tempdirs[object_id])
-        @tempdirs[object_id] = nil
+        @tempdirs.delete(object_id)
         dirs_to_remove.each do |dir|
           ::FileUtils.remove_entry(dir)
         end
@@ -10,16 +10,16 @@ module Roo
     end
 
     def make_tempdir(object, prefix, root)
-      root ||= ENV['ROO_TMP']
+      root ||= ENV["ROO_TMP"]
       # folder is cleaned up in .finalize_tempdirs
       ::Dir.mktmpdir("#{Roo::TEMP_PREFIX}#{prefix}", root).tap do |tmpdir|
-        @tempdirs ||= {}
-        if @tempdirs[object.object_id]
-          @tempdirs[object.object_id] << tmpdir
-        else
-          @tempdirs[object.object_id] = [tmpdir]
+        @tempdirs ||= Hash.new { |h, k| h[k] = [] }
+
+        if @tempdirs[object.object_id].empty?
           ObjectSpace.define_finalizer(object, method(:finalize_tempdirs))
         end
+
+        @tempdirs[object.object_id] << tmpdir
       end
     end
   end
