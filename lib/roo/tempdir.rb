093b9879aafef05ae8b34388ce1a7e51e791e705
Todd Blackman <tdblackman@gmail.com>
2018-08-23 13:44:57 -0400

    Fix problems with roo temp files disappearing before a process was done with them.
    Instead of removing temp files when the GC cleans up a roo object, just
    clean up old files from previous runs.

--- lib/roo/tempdir.rb
+++ lib/roo/tempdir.rb
@@ -3,8 +3,21 @@ module Roo
     def finalize_tempdirs(object_id)
       if @tempdirs && (dirs_to_remove = @tempdirs[object_id])
         @tempdirs.delete(object_id)
-        dirs_to_remove.each do |dir|
-          ::FileUtils.remove_entry(dir)
+
+        # Ignore the directory list since it seems this method gets called too
+        # prematurely, related to the object being garbage collected in a
+        # different process (sidekiq/puma for example)
+
+        cmds = [
+          # Delete roo temporary directories over 15 minutes old.
+          "find #{ENV["ROO_TMP"]||'/tmp'} -type d -name '#{Roo::TEMP_PREFIX}*' -mmin +15 -exec rm -r {} \\;",
+
+          # Uploaded files (I think from roo)
+          "find #{ENV["ROO_TMP"]||'/tmp'} -name 'RackMultipart*' -mmin +15 -exec rm -r {} \\;"
+        ]
+
+        cmds.each do |cmd|
+          system(cmd)
         end
       end
     end
