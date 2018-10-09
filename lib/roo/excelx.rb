d68f002cb1786572aadf6c0465eb9872c9bfe8c5
chopraanmol1 <chopraanmol1@gmail.com>
2018-10-09 10:56:33 +0530

    Option to skip allocation for empty cell.
    This option skips creation of empty cell and assignment of key, value pair for such empty cell in hash.

--- lib/roo/excelx.rb
+++ lib/roo/excelx.rb
@@ -40,8 +40,9 @@ module Roo
       sheet_options = {}
       sheet_options[:expand_merged_ranges] = (options[:expand_merged_ranges] || false)
       sheet_options[:no_hyperlinks] = (options[:no_hyperlinks] || false)
+      sheet_options[:empty_cells] = (options[:empty_cells] || false)
       shared_options = {}
-        
+
       shared_options[:disable_html_wrapper] = (options[:disable_html_wrapper] || false)
       unless is_stream?(filename_or_stream)
         file_type_check(filename_or_stream, %w[.xlsx .xlsm], 'an Excel 2007', file_warning, packed)
